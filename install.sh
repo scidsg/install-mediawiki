d12a6bd286eb80c9ddb0785ffa640f8f49a0f430
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-01 14:42:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt -y dist-upgrade
 sudo apt -y autoremove
 
 # Install necessary packages
-sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql php-xml php-mbstring php-apcu php-intl imagemagick php-gd php-cli curl php-curl git
+sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql php-xml php-mbstring php-apcu php-intl imagemagick php-gd php-cli curl php-curl git whiptail
 
 # Get the latest MediaWiki version and tarball URL
 MW_TARBALL_URL=$(curl -s https://www.mediawiki.org/wiki/Download | grep -oP '(?<=href=")[^"]+(?=\.tar\.gz")' | head -1)
@@ -47,19 +47,16 @@ sudo a2dissite 000-default
 # Restart Apache
 sudo systemctl restart apache2
 
-echo "Please enter your desired database name for MediaWiki: "
-read db_name
-echo "Please enter your desired database username for MediaWiki: "
-read db_user
-echo "Please enter your desired database password for MediaWiki: "
-read -s db_pass
+# Get user input using whiptail
+db_name=$(whiptail --inputbox "Please enter your desired database name for MediaWiki:" 8 78 --title "Database Name" 3>&1 1>&2 2>&3)
+db_user=$(whiptail --inputbox "Please enter your desired database username for MediaWiki:" 8 78 --title "Database Username" 3>&1 1>&2 2>&3)
+db_pass=$(whiptail --passwordbox "Please enter your desired database password for MediaWiki:" 8 78 --title "Database Password" 3>&1 1>&2 2>&3)
 
 # Create the database and user
 sudo mysql -e "CREATE DATABASE \`${db_name}\` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 sudo mysql -e "CREATE USER '${db_user}'@'localhost' IDENTIFIED BY '${db_pass}';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON \`${db_name}\`.* TO '${db_user}'@'localhost';"
 sudo mysql -e "FLUSH PRIVILEGES;"
-
 
 # Done
 echo "MediaWiki has been installed. Please visit http://mediawiki.local to complete the setup and apply the Vector skin."
