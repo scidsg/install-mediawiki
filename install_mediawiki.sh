c52a7848fd0b5848224e42289db2e9bbb8a8e65e
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-01 11:43:36 -0700

    Update install_mediawiki.sh

--- install_mediawiki.sh
+++ install_mediawiki.sh
@@ -8,12 +8,15 @@ sudo apt -y autoremove
 # Install necessary packages
 sudo apt install -y apache2 php libapache2-mod-php mariadb-server php-mysql php-xml php-mbstring php-apcu php-intl imagemagick php-gd php-cli php-curl git
 
+# Get the latest MediaWiki version
+MW_VERSION=$(curl -s https://api.github.com/repos/wikimedia/mediawiki/tags | grep -oP '(?<="name": "REL)[^"]+' | head -1 | sed 's/^/1./')
+
 # Download MediaWiki
-wget https://releases.wikimedia.org/mediawiki/1.39/mediawiki-1.39.3.tar.gz
-tar xvzf mediawiki-1.39.3.tar.gz
+wget https://releases.wikimedia.org/mediawiki/${MW_VERSION%.*}/mediawiki-${MW_VERSION}.tar.gz
+tar xvzf mediawiki-${MW_VERSION}.tar.gz
 
 # Move MediaWiki to the web server directory
-sudo mv mediawiki-1.39.3 /var/www/html/mediawiki
+sudo mv mediawiki-${MW_VERSION} /var/www/html/mediawiki
 
 # Set appropriate permissions
 sudo chown -R www-data:www-data /var/www/html/mediawiki
